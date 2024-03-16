270ba681980249f6bf17e528f21c290cf6ec4bd9
KanybekovMaksat <maksatkanybekov2004@gmail.com>
2024-03-16 22:12:03 +0600

    feat:create header component, downloaded icon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Header from '../components/Header';
 
 const Home = () => {
   return (
-    <div className='mobile-content'>
+    <div className="mobile-content">
+      <Header />
       <Navigation />
     </div>
   );
