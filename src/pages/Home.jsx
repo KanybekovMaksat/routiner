66952f3bfefdc6e7e55097781f2634a1a9231b97
KanybekovMaksat <maksatkanybekov2004@gmail.com>
2024-03-19 10:08:03 +0600

    feat: create pages and configured route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
-import Header from '../components/Header';
 import TopContent from '../components/TopContent';
 import SegmentControl from '../components/SegmentControl/SegmentControl';
 import DailyCard from '../components/DailyCard';
@@ -8,7 +7,6 @@ import DailyCard from '../components/DailyCard';
 const Home = () => {
   return (
     <div className="mobile-content">
-      <Header />
       <TopContent />
       <SegmentControl />
       <DailyCard />
--- src/pages/router.js
+++ src/pages/router.js
@@ -1,11 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import ExplorePage from './Explore';
+import ActivityPage from './Activity';
+import ProfilePage from './Profile';
+import NotFoundPage from './NotFound';
 
 const Router = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/activity" element={<ActivityPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
